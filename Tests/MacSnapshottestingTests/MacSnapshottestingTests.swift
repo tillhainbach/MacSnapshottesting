8b5d6f7864ed3435869ea4b90c155d1417997498
Till Hainbach <Till.Hainbach@posteo.de>
2021-11-02 11:56:40 +0100

    fix: transform colorspace and sizeto 1.0

--- Tests/MacSnapshottestingTests/MacSnapshottestingTests.swift
+++ Tests/MacSnapshottestingTests/MacSnapshottestingTests.swift
@@ -1,31 +1,113 @@
+import CoreGraphics
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SnapshotTesting
 import SwiftUI
 import XCTest
 @testable import MacSnapshottesting
 
 final class MacSnapshottestingTests: XCTestCase {
+
   func testExample() throws {
     let view = MacSnapshottesting()
-
+//    isRecording = false
     assertSnapshot(matching: view, as: .image)
 
   }
 }
 
-extension Snapshotting where Value: SwiftUI.View, Format == NSImage {
-  static var image: Self {
-    Snapshotting<NSView, NSImage>.image()
-      .pullback { swiftUIView in
-        let controller = NSHostingController(rootView: swiftUIView)
-        let view = controller.view
-        view.window?.colorSpace = .genericCMYK
-        view.frame.size = .init(width: 500, height: 300)
-        view.window?.convertToBacking(view.frame)
-        view.window?.backgroundColor = .darkGray
+extension Snapshotting where Value: View, Format == NSImage {
 
+  public static var image: Self {
+    return SimplySnapshotting.image(precision: 1).asyncPullback { swiftUIView in
+      let controller = NSHostingController(rootView: swiftUIView)
+      let view = controller.view
+      let size = NSSize(width: 500, height: 300)
+      view.frame.size = size
 
-        return view
+      return Async { callback in
+        let image =  NSImage(data: view.dataWithPDF(inside: view.bounds))!
+        let cgImage = image.cgImage(forProposedRect: nil, context: nil, hints: nil)!
+        let genericRGBColorSpace = CGColorSpace(name: CGColorSpace.genericRGBLinear)!
+        let ciImage = CIImage(cgImage: cgImage).matchedFromWorkingSpace(to: genericRGBColorSpace)!
+        let resizeFilter = CIFilter.lanczosScaleTransform()
+        resizeFilter.scale = Float(size.width / ciImage.extent.width)
+        resizeFilter.inputImage = ciImage
+        let ouputImage = resizeFilter.outputImage!
+        let context = CIContext(options: nil)
+        let newCGImage = context.createCGImage(ouputImage, from: ouputImage.extent)!
+        let genericImage = NSImage(
+          cgImage: newCGImage,
+          size: .init(width: newCGImage.width, height: newCGImage.height)
+        )
+
+        callback(genericImage)
+
       }
+
+    }
   }
+
+  public static func image(precision: Float = 1, size: CGSize? = nil) -> Snapshotting {
+    return SimplySnapshotting.image(precision: precision).asyncPullback { swiftUIView in
+      let controller = NSHostingController(rootView: swiftUIView)
+      let window = NSWindow(contentViewController: controller)
+      window.backgroundColor = .windowBackgroundColor
+      window.colorSpace = .genericRGB
+      let windowController = NSWindowController(window: window)
+
+      return Async { callback in
+        windowController.showWindow(self)
+        DispatchQueue.main.asyncAfter(deadline: .now().advanced(by: .seconds(1))) {
+          guard let image = takeSnapshot(from: windowController) else { return }
+          callback(image)
+        }
+      }
+    }
+  }
+
+  static private func takeSnapshot(from windowController: NSWindowController) -> NSImage? {
+    guard let window = windowController.window else { return nil }
+
+    var image = CGWindowListCreateImage(
+      .null,
+      .optionIncludingWindow, CGWindowID(window.windowNumber), [.shouldBeOpaque, .boundsIgnoreFraming])!
+    let genericRGBColorSpace = CGColorSpace(name: CGColorSpace.genericRGBLinear)!
+    image = image.copy(colorSpace: genericRGBColorSpace)!
+
+    return NSImage(cgImage: image, size: .init(width: image.width, height: image.height))
+
+  }
+
+
+//  static private func convertToRGB(cgImage: CGImage) -> NSImage {
+//    guard
+//      let sourceImageFormat = vImage_CGImageFormat(cgImage: cgImage),
+//      let rgbDestinationImageFormat = vImage_CGImageFormat(
+//        bitsPerComponent: 8,
+//        bitsPerPixel: 32,
+//        colorSpace: CGColorSpaceCreateDeviceRGB(),
+//        bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.first.rawValue),
+//        renderingIntent: .defaultIntent)
+//    else {
+//      print("Unable to initialize cgImage or colorSpace.")
+//      return nil
+//    }
+//
+//    guard
+//      let sourceBuffer = try? vImage_Buffer(cgImage: cgImage),
+//      var rgbDestinationBuffer = try? vImage_Buffer(
+//        width: Int(sourceBuffer.width),
+//        height: Int(sourceBuffer.height),
+//        bitsPerPixel: rgbDestinationImageFormat.bitsPerPixel)
+//    else {
+//      XCTFail("Error initializing source and destination buffers.")
+//    }
+//
+//    defer {
+//      sourceBuffer.free()
+//      rgbDestinationBuffer.free()
+//    }
+//  }
 
 }
